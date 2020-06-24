1e6e7f4a09b8a4d66d6ff66368346bac7fe98f30
Rene Pereira <rpereira@CLOSEUPBRZ.COM.BR>
2020-06-24 00:01:58 -0300

    ajustando para standalone mostrar redux tools

--- container/src/store/index.js
+++ container/src/store/index.js
@@ -2,7 +2,7 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import Thunk from 'redux-thunk';
 import rootReducer from 'Reducers';
 const middlewares = [Thunk];
-const composeEnhancers =  process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  || compose : compose;
+const composeEnhancers =  process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'standalone' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  || compose : compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
 
 export default store;
